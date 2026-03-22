096b62104643b6d60439d37c082c6e224b44674b
Jegadhesan R <jegadhesan21@gmail.com>
2026-03-22 16:27:30 +0530

    update13

--- backend/config/db.js
+++ backend/config/db.js
@@ -18,6 +18,5 @@ const db = pool.promise();
 db.getConnection()
     .then(() => console.log('Database connected successfully!'))
     .catch(err => console.error('Database connection failed:', err.message));
-
     
 module.exports = db;
